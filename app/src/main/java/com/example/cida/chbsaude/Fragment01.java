ad036c1d28b4aa307a9e326e77d1dc1376e0a8e0
maria <cidamuniz2011@yahoo.com.br>
2017-07-16 10:40:33 -0300

    inicial commit

---
+++ app/src/main/java/com/example/cida/chbsaude/Fragment01.java
@@ -0,0 +1,21 @@
+package com.example.cida.chbsaude;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+
+
+public class Fragment01 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment01, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/cida/chbsaude/Fragment02.java
@@ -0,0 +1,16 @@
+package com.example.cida.chbsaude;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+public class Fragment02 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment02, container, false);
+    }
+
+}
