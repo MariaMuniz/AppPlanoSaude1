247ba4b1bfef1c678c3c62ab27cd13af8ee7dcd0
maria <cidamuniz2011@yahoo.com.br>
2017-07-19 13:35:34 -0300

    terca

--- app/src/main/java/com/example/cida/chbsaude/Fragment01.java
+++ app/src/main/java/com/example/cida/chbsaude/Fragment01.java
@@ -7,15 +7,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.Fragment;
-
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 
 public class Fragment01 extends Fragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+ //   public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                    //         Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment01, container, false);
+       // return inflater.inflate(R.layout.fragment_fragment01, container, false);
+
+
+
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_fragment01, container, false);
+
+        String [] values =
+                {"Familiar","Individual","Odontológico","Empresarial","Ambulatorial"};
+        Spinner spinner = (Spinner) v.findViewById(R.id.spnPlano);
+        ArrayAdapter<String> LTRadapter = new ArrayAdapter<String>(this.getActivity(),R.layout.support_simple_spinner_dropdown_item, values);
+        LTRadapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(LTRadapter);
+
+
+        return v;
+
+
     }
 
 }
