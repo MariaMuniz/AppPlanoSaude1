8c265f090abc393b65eaf197a1ca2e7835b8ced2
maria <cidamuniz2011@yahoo.com.br>
2017-07-19 21:50:17 -0300

    terca4

--- app/src/main/java/com/example/cida/chbsaude/Fragment01.java
+++ app/src/main/java/com/example/cida/chbsaude/Fragment01.java
@@ -11,36 +11,40 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class Fragment01 extends Fragment {
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment01, container, false);
+
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_fragment01, container, false);
+
+        String [] values =
+                {"2014","2015","2016"};
+        Spinner spinner = (Spinner) v.findViewById(R.id.spnPlano);
+        ArrayAdapter<String> LTRadapter = new ArrayAdapter<String>(this.getActivity(),R.layout.support_simple_spinner_dropdown_item, values);
+        LTRadapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(LTRadapter);
+
+
+        return v;
+
+
+
+
+
+
     }
-
-
-
-    public class Plano
-    {
-        public ArrayList<String> RetornarPlano;
-
-        public Plano()
-        {
-            RetornarPlano = new ArrayList<String>();
-            RetornarPlano.add("Individual");
-            RetornarPlano.add("Odontológico");
-            RetornarPlano.add("Familiar");
-            RetornarPlano.add("Empresarial");
-            RetornarPlano.add("Ambolatorial");
-            RetornarPlano.add("Adesão Alfix");
-            RetornarPlano.add("Adesão Allcare");
-            RetornarPlano.add("Adesão Bem Benefícios");
-            RetornarPlano.add("Adesão Qualicorp");
-
-        }
-    }
-
 }
